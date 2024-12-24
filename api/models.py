4e3a9b5082f1b61bd9cfb4603e282c6ed14fe62a
rhashhanok <rhashhanok220@mail.ru>
2024-12-25 02:22:54 +0300

    Add models.py

--- api/models.py
+++ api/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class TestModel(models.Model):
     name = models.CharField(max_length=155)
-    age = models
+    age = models.IntegerField()
+
+
+class TestModel2(models.Model):
+    name = models.CharField(max_length=155)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import TestViewSet
+from .views import TestViewSet, TestModel2ViewSet
 
 router = routers.SimpleRouter()
 router.register(r'test', TestViewSet)
+router.register(r'test2', TestModel2ViewSet)
 urlpatterns = [
     path('', include(router.urls))
 ]
