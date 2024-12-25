fb98f823b4e06224a15ee7aef7fa29d6194084a9
rhashhanok <rhashhanok220@mail.ru>
2024-12-25 18:14:44 +0300

    update settings.py CSRF_COOKIE_SECURE

--- TestDj/settings.py
+++ TestDj/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -27,7 +26,8 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://testdj-production.up.railway.app/', 'https://testdj-production.up.railway.app/debit-client/']
+CSRF_TRUSTED_ORIGINS = ['https://testdj-production.up.railway.app/',
+                        'https://testdj-production.up.railway.app/debit-client/']
 
 # Application definition
 
@@ -74,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TestDj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -84,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -116,7 +113,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -127,6 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 TEMPLATE_DIR = [os.path.join(BASE_DIR, "templates")]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
