8eb2b9bcf7dcca3828fa71d1d946b072f036337e
rhashhanok <rhashhanok220@mail.ru>
2024-12-25 04:31:46 +0300

    tkinter test

--- ut/views.py
+++ ut/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from api.models import TestModel
+from tkinter import filedialog
+
+test = False
 
 
 def index(request):
     context = {
         'data': TestModel.objects.all()
     }
+    if test:
+        f = filedialog.askopenfilename()
     return render(request, 'ut/home.html', context=context)
